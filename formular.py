66e98d1b66a6722ca4323ab3082c35a79c75338f
eyalzek <eyal.ez@gmail.com>
2015-06-13 20:49:27 +0200

    initial commit

---
+++ formular.py
@@ -0,0 +1,132 @@
+import time
+import json
+import sys
+from selenium import webdriver
+from selenium.common import exceptions
+
+
+SLEEP_INTERVAL = 1800
+
+
+class Webpage(object):
+    """A headless PhantomJS page running the formular page"""
+    def __init__(self, info):
+        super(Webpage, self).__init__()
+        self.info = info
+        self.driver = webdriver.Chrome()
+        self.driver.set_window_size(1366, 768)
+        self.url = "https://formular.berlin.de/xima-forms-29/get/14278898950750000?mandantid=/OTVBerlin_LABO_XIMA/000-01/instantiationTasks.properties"
+
+    def run_flow(self):
+        self.driver.get(self.url)
+        self.print_title()
+        self.screenshot("homepage")
+        self.click_button("#btnTerminBuchen")  # schedule a meeting
+        self.make_selection("#cobStaat", "441")  # set country in the DD
+        self.make_selection("#cobFamAngInBerlin", "Nein")  # answer 'No' in the second DD
+        self.make_selection("#cobAnliegen", "305156")  # select type of visa wanted
+        self.make_selection("#cbZurKenntnis", "true", "click", "checked")  # check terms and conditions
+        self.click_button("#labNextpage")  # click on next page button
+        self.make_selection("#tfFirstName", self.info["firstname"], "blur")  # enter first name
+        self.make_selection("#tfLastName", self.info["lastname"], "blur")  # enter last name
+        self.make_selection("#cobGebDatumTag", self.info["day"])  # enter day of birth
+        self.make_selection("#cobGebDatumMonat", self.info["month"])  # enter month of birth
+        self.make_selection("#tfGebDatumJahr", self.info["year"], "blur")  # enter year of birth
+        self.make_selection("#cobVPers", "1")  # enter number of persons applying
+        self.make_selection("#tfMail", self.info["email"], "blur")  # enter e-mail address
+        self.make_selection("#cobGenehmigungBereitsVorhanden", "Nein")  # "do you already have a residence permit?"
+        self.click_button("#txtNextpage")  # click on next page button
+
+    def select_month(self, month, year):
+        while True:
+            m = self.wait_for_visibility("#month")
+            y = self.wait_for_visibility("#year")
+            if (m.text.strip(), y.text.strip()) == (month, year):
+                break
+            self.click_button("#labnextMonth")
+
+    def check_availability(self, start_day, end_day):
+        available_dates = self.driver.find_elements_by_css_selector(".CELL a[link='1']")
+        return [date.text for date in available_dates if int(date.text) in range(start_day, end_day)]
+
+    def click_button(self, selector):
+        tag = self.wait_for_visibility(selector)
+        tag.click()
+        self.screenshot(selector)
+        self.print_title()
+
+    def make_selection(self, selector, value, event="change", attribue="value"):
+        self.wait_for_visibility(selector)  # make sure element is visible
+        # print("COMMAND:")
+        # print('document.querySelector("%s").%s = "%s"' % (selector, attribue, value))
+        self.driver.execute_script('document.querySelector("%s").%s = "%s"' % (selector, attribue, value))
+        # print("COMMAND:")
+        # print('document.querySelector("%s").dispatchEvent(new Event("%s"))' % (selector, event))
+        self.driver.execute_script('document.querySelector("%s").dispatchEvent(new Event("%s"))' % (selector, event))
+
+    def wait_for_visibility(self, selector, timeout_seconds=5):
+        retries = timeout_seconds
+        pause_interval = 2
+        while retries:
+            # print("tries left: {}".format(retries))
+            try:
+                element = self.driver.find_element_by_css_selector(selector)
+                if element.is_displayed():
+                    return element
+                elif "visible" in element.value_of_css_property("visibility"):
+                    print("trying to focus on element")
+                    self.driver.execute_script("$(\"" + selector + "\").focus()")
+            except (exceptions.NoSuchElementException,
+                    exceptions.StaleElementReferenceException):
+                if retries <= 0:
+                    raise
+                else:
+                    pass
+
+            retries = retries - 1
+            time.sleep(pause_interval)
+        raise exceptions.ElementNotVisibleException(
+            "Element {} not visible despite waiting for {} seconds".format(
+                selector, timeout_seconds * pause_interval)
+        )
+
+    def print_title(self):
+        print("Page title:")
+        print(self.driver.title.encode('utf-8'))
+
+    def screenshot(self, filename):
+        print("saving %s" % filename)
+        self.driver.save_screenshot(filename + ".png")
+
+    def quit(self):
+        print("closing page...")
+        self.driver.quit()
+
+
+def get_info(filename):
+    with open(filename, "rb") as f:
+        return json.loads(f.read())
+
+
+def main(filename):
+    info = get_info(filename)
+    print(info)
+    while True:
+        page = Webpage(info)
+        page.run_flow()
+        page.select_month(info["wantedmonth"], info["wantedyear"])
+        print("Checking for available dates....")
+        result = page.check_availability(int(info["startrange"]), int(info["endrange"]))
+        if len(result) > 0:
+            print("Available date found in the given range!")
+            print("Available dates are:")
+            print(result)
+            break
+        print("No available dates in the given range...")
+        print("Sleeping for %d" % SLEEP_INTERVAL)
+        page.quit()
+        time.sleep(SLEEP_INTERVAL)
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+    main(sys.argv[1])
---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    util = require('util'),
+    spawn = require('child_process').spawn,
+    tmp = require('tmp'),
+    bodyParser = require('body-parser');
+
+app.use(express.static('src'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.sendFile('src/index.html');
+});
+
+app.post('/enter-info', function(req, res) {
+    console.log('received personal info')
+    console.log(req.body)
+    tmp.file(function _tempFileCreated(err, path, fd, cleanupCallback) {
+        if (err) throw err;
+        fs.writeFile(path, JSON.stringify(req.body), function (err) {
+            if (err) throw err;
+            console.log('info save as: ', path);
+            // run worker:
+            proc = spawn('python', ['formular.py', path]);
+            proc.stdout.on('data', function (data) {
+              console.log('stdout: ' + data);
+            });
+
+            proc.stderr.on('data', function (data) {
+              console.log('stderr: ' + data);
+            });
+
+            proc.on('exit', function (code) {
+              console.log('child process exited with code ' + code);
+            });
+        });
+    });
+    res.send('Info received!')
+});
+
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
